98ec406535ac2ae05df4dd0e63d923dace31c7d9
denysLerroan <lerroandeco@gmail.com>
2020-10-26 21:02:38 -0300

    Routes have been defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SenatorsDetailsComponent } from './senators-details/senators-details.component';
+import { SenatorsListComponent } from './senators-list/senators-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'senadores', component: SenatorsListComponent },
+  { path: 'senadores/:id', component: SenatorsDetailsComponent },
+  { path: '', redirectTo: 'senadores', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
