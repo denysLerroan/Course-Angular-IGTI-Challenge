87e5d9217ff5845f3cd2ca221418fa9d3fc52d19
denysLerroan <lerroandeco@gmail.com>
2020-10-31 01:48:15 -0300

    Defining expenses types

--- src/app/senators.service.ts
+++ src/app/senators.service.ts
@@ -5,6 +5,16 @@ import { Senators } from './senators';
 
 const urlBase = 'http://localhost:3000';
 
+export const types = {
+  1: 'Aluguel de imóveis e despesas concernentes a eles',
+  2: 'Divulgação de atividade parlamentar',
+  3: 'Aquisição de material de consumo para uso no escritório',
+  4: 'Passagens aéreas, aquáticas e terrestres nacionais',
+  5: 'Contratação de consultorias, acessorias, pesquisas, trabalhos técnicos e outros serviços',
+  6: 'Locomoção, gospedagem, alimentação e combustíveis',
+  7: 'Serviços de Segurança Privada',
+};
+
 @Injectable({
   providedIn: 'root',
 })
